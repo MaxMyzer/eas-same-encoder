46c1470953b575111906b4242b3be39e197eac73
mm12 <81774156+MaxMyzer@users.noreply.github.com>
2021-04-30 23:06:02 -0500

    Updated same.py
    This should let you do a lot more

--- same.py
+++ same.py
@@ -8,11 +8,45 @@ import argparse
 
 # parse command-line arguments
 parser = argparse.ArgumentParser()
+parser.add_argument("--code", "-c", nargs='?', default="")
 parser.add_argument("--playaudiolive", "-pal", nargs='?', default=-1)
-parser.add_argument("--code", "-c", nargs='?', default="none")
+parser.add_argument("--org", "-o", nargs='?', default="WXR")
+parser.add_argument("--event", "-e", nargs='?', default="RWT")
+parser.add_argument("--location", "-lo", nargs='?', default=None)
+parser.add_argument("--location00", "-l00", nargs='?', default="000000")
+parser.add_argument("--location01", "-l01", nargs='?', default=None)
+parser.add_argument("--location02", "-l02", nargs='?', default=None)
+parser.add_argument("--location03", "-l03", nargs='?', default=None)
+parser.add_argument("--location04", "-l04", nargs='?', default=None)
+parser.add_argument("--location05", "-l05", nargs='?', default=None)
+parser.add_argument("--location06", "-l06", nargs='?', default=None)
+parser.add_argument("--location07", "-l07", nargs='?', default=None)
+parser.add_argument("--location08", "-l08", nargs='?', default=None)
+parser.add_argument("--location09", "-l09", nargs='?', default=None)
+parser.add_argument("--location10", "-l10", nargs='?', default=None)
+parser.add_argument("--location11", "-l11", nargs='?', default=None)
+parser.add_argument("--location12", "-l12", nargs='?', default=None)
+parser.add_argument("--location13", "-l13", nargs='?', default=None)
+parser.add_argument("--location14", "-l14", nargs='?', default=None)
+parser.add_argument("--location15", "-l15", nargs='?', default=None)
+parser.add_argument("--location16", "-l16", nargs='?', default=None)
+parser.add_argument("--location17", "-l17", nargs='?', default=None)
+parser.add_argument("--location18", "-l18", nargs='?', default=None)
+parser.add_argument("--location19", "-l19", nargs='?', default=None)
+parser.add_argument("--location20", "-l20", nargs='?', default=None)
+parser.add_argument("--location21", "-l21", nargs='?', default=None)
+parser.add_argument("--location22", "-l22", nargs='?', default=None)
+parser.add_argument("--location23", "-l23", nargs='?', default=None)
+parser.add_argument("--location24", "-l24", nargs='?', default=None)
+parser.add_argument("--location25", "-l25", nargs='?', default=None)
+parser.add_argument("--location26", "-l26", nargs='?', default=None)
+parser.add_argument("--location27", "-l27", nargs='?', default=None)
+parser.add_argument("--location28", "-l28", nargs='?', default=None)
+parser.add_argument("--location29", "-l29", nargs='?', default=None)
+parser.add_argument("--location30", "-l30", nargs='?', default=None)
+parser.add_argument("--time", "-t", nargs='?', default="0015")
+parser.add_argument("--callsign", "-cs", nargs='?', default="KEAX/NWS")
 args = parser.parse_args()
-
-
 ######## CONFIG / constants ########
 
 markBitFrequency = 2083 + (1/3)
@@ -30,7 +64,7 @@ fs = 43750
 
 
 # f = 2083.33333
-	
+
 samples = np.zeros(0)
 
 
@@ -40,7 +74,7 @@ def markBit():
 	# f = 2083.33333
 	f = markBitFrequency
 	t = 1.0 / (520 + (5/6))
-	
+
 	samples = np.arange(t * fs) / fs
 
 	roffle = np.sin(2 * np.pi * f * samples)
@@ -52,7 +86,7 @@ def spaceBit():
 	# f = 1562.5
 	f = spaceBitFrequency
 	t = 1.0 / (520 + (5/6))
-	
+
 	samples = np.arange(t * fs) / fs
 
 	return np.sin(2 * np.pi * f * samples)
@@ -107,46 +141,27 @@ def preamble():
 	return byte_data
 
 
-
-
-# ZCZC-WXR-RWT-020103-020209-020091-020121-029047-029165-029095-029037+0030-1051700-KEAX/NWS
-
-# code = "ZCZC-EAS-RMT-011000+0100-2141800-SCIENCE-"
-# code = "ZCZC-WXR-TOR-000000+0030-2142200-SCIENCE -"
-# code = "ZCZC-PEP-EAN-000000+0400-2142350-SCIENCE -"
-
-# control string
-# code = "ZCZC-EAS-RMT-011000+0100-2142200-KMMS FM -"
-
-# useful FIPS codes
-# 000000 - the whole fucking united states
-# 024031 - silver spring, md / montgomery county
-# 011001 - district of columbia
-
 # EAS alerts are heavily dependent on timestamps so this makes it easy/fun to send a thing now
 sameCompatibleTimestamp = datetime.datetime.now().strftime("%j%H%M")
 
-# known good
-# OH SHIT it's all time-dependent
-# which i can now do since the time works on the box
-#code = "ZCZC-PEP-EAN-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -"  # nuclear armageddon (or some other form of "we are all likely to die")
-# code = "ZCZC-PEP-EAT-000000+0400-" + sameCompatibleTimestamp + "-SCIENCE -"  # nuclear armageddon (or some other form of "we are all likely to die")
-# code = "ZCZC-PEP-EAT-000000+0400-2142350-SCIENCE -"  # lol jk no nuclear armageddon
-code = "ZCZC-WXR-TOR-024031+0030-" + sameCompatibleTimestamp + "-SCIENCE -"  # tornado warning, silver spring, md
-# code = "ZCZC-WXR-SVR-024031+0030-2142200-SCIENCE -"  # severe thunderstorm warning, silver spring, md
-# code = "ZCZC-WXR-EVI-024031+0030-" + sameCompatibleTimestamp + "-SCIENCE -"  # evacuation immediate!!, silver spring, md
-# code = "ZCZC-WXR-FFW-024031+0030-2150021-SCIENCE -"
-
-# testing
-# code = "ZCZC-CIV-LAE-024031+0030-2150022-SCIENCE -"
-# code = "ZCZC-CIV-CDW-024031+0400-" + sameCompatibleTimestamp + "-SCIENCE -"
-# code = "ZCZC-PEP-EAN-024031+0030-" + sameCompatibleTimestamp + "-SCIENCE -"
-
-# code = "ZCZC-ROFL-WTF-012345+0000-YO WADDUP MOTHAFUCKAZ="
-
-
-code = args.code
-
+LocationList = ""
+# arguments are location.00 to location.30
+if args.location != None:
+	LocationList = "-" + args.location
+else:
+	Where = (args.location00, args.location01, args.location02, args.location03, args.location04, args.location05, args.location06, args.location07, args.location08, args.location09, args.location10,args.location11,args.location12, args.location13, args.location14, args.location15, args.location16, args.location17, args.location18, args.location19, args.location20, args.location21, args.location22, args.location23, args.location24, args.location25, args.location26, args.location27, args.location28, args.location29, args.location30)
+	# LocationList = "-".join(Where)
+	for i in range(0, 30):
+		if Where[i] != None:
+			LocationList+= "-" + Where[i]
+		else:
+			LocationList+= ""
+if LocationList == "":
+	LocationList = "000000"
+print(LocationList)
+code = "ZCZC" + "-" + args.org + "-" + args.event + "" + LocationList + "+" + args.time +  "-" + sameCompatibleTimestamp + "-" + args.callsign + "-"
+if args.code != "" and args.code != None and args.code != " ":
+	code = args.code
 
 
 for i in range(0, 3):
@@ -167,14 +182,14 @@ for i in range(0, 3):
 	# signal = np.append(signal, extramarks(10))
 	signal = np.append(signal, preamble())
 
-	for char in "NNNN": # NNNN = End Of Message
+	for char in "ZCZCNNNN": # NNNN = End Of Message
 		signal = np.append(signal, byte(char))
 
 	# signal = np.append(signal, extramarks(6))
 
 	signal = np.append(signal, np.zeros(43750)) # wait the requisite one second
 
-	
+
 
 signal *= 32767
 
@@ -185,5 +200,3 @@ wavfile.write(str("same.wav"), fs, signal)
 
 if args.playaudiolive == "1":
 	subprocess.call("afplay same.wav", shell=True)
-
-
